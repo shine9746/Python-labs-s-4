e63471b355867e838ab318a49dfc5fcd7c158b07
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-22 11:21:16 +0530

    Update Python-lab-s4-6.py

--- Python-lab-s4-6/Python-lab-s4-6.py
+++ Python-lab-s4-6/Python-lab-s4-6.py
@@ -1,11 +1,6 @@
 #Python programme to print sum of all numbers in a list
 
 list = [10,20,30,40,60]
-add=0
-
-for i in range(len(list)):
-        add += list[i]
-
 
 print(list)
 print("sum of list: ",sum(list))
