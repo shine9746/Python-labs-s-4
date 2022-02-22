ef1678b29a8e98989eae7dc3b892cb801be59bf2
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-22 10:24:06 +0530

    Update Python-lab-s4-1.py

--- Python-lab-s4-1/Python-lab-s4-1.py
+++ Python-lab-s4-1/Python-lab-s4-1.py
@@ -9,12 +9,12 @@ print("Current year : ",current.strftime("%Y"))
 
 print("Month of year : ",current.strftime("%B"))
 
-print("Week number of the year : ",current.strftime("%W"))
+print("Week number of the year : ",current.strftime("%U"))
 
-print("Weekday of the week : ",current.strftime("%w"))
+print("Weekday of the week : ",current.strftime("%A"))
 
 print("Day of year : ",current.strftime("%j"))
 
 print("Day of the month : ",current.strftime("%d"))
 
-print("Day of week : ",current.strftime("%A"))
+print("Day of week : ",current.strftime("%w"))
